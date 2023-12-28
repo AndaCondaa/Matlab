4f208937ce1cefbd3d24472d432b548b60af58ea
AndaCondaa <simon5229@naver.com>
2023-12-29 00:02:36 +0900

    1228

---
+++ a_plus_b.m
@@ -0,0 +1,13 @@
+% Making Function
+
+% function
+% ...
+% ...  
+% end
+
+% ******* 파일명과 함수명을 동일하게 저장
+
+function y = a_plus_b(x1, x2)
+    y = x1 + x2;  % 세미콜론 입력 시, y 계산결과 생략
+end
+
---
+++ for_test.m
@@ -0,0 +1,6 @@
+total = 0;
+
+% Start:Shift(생략 시, 1씩 증가):End 
+for i = 1:1:10
+    total = total + i
+end
