bf829cfa2800011663f9398e6eb4899e99f58666
AndaCondaa <simon5229@naver.com>
2024-01-01 01:30:42 +0900

    0101

--- Basic/textRead_test.m
+++ Basic/textRead_test.m
@@ -1,1 +1,1 @@
-textread('C:\Users\simon\OneDrive\바탕 화면\Simon\Matlab\Basic\text.txt', )
+[a, b, c] = textread('C:\Users\simon\OneDrive\바탕 화면\Simon\Matlab\Basic\text.txt', '%u %u %c', 'headerlines', 1, 'delimiter', ',');
